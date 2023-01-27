588c8fa484b197c43fdf9951a8aa93d9a922c055
Kailash Nath Yadav <107490624+KailashNathYadav@users.noreply.github.com>
2023-01-27 10:05:00 +0530

    Add files via upload

---
+++ Construction.java
@@ -0,0 +1,34 @@
+package com.prob1.service;
+import java.util.*;
+
+public class Construction {
+	private int totalFloor;
+	List<Integer> floors = new ArrayList<>();
+	Set<Integer> constructed = new HashSet<>();
+	Queue<Integer>  maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+	Scanner sc = new Scanner(System.in);
+	
+	public void getData() {
+		System.out.println("Enter the total number of floors in the building");
+		totalFloor = sc.nextInt();
+		for(int i = 0; i < totalFloor; i ++) {
+			System.out.println("Enter the floor size given on day : " + (i + 1));
+			floors.add(sc.nextInt());
+		}
+		sc.close();
+		for(int f: floors)	  maxHeap.add(f);
+		System.out.println();
+	}
+	
+	public void assembledFloor() {
+		System.out.println("The order of construction is as follows\n");
+		for(int i = 0; i < totalFloor; i ++) { 
+			constructed.add(floors.get(i));
+			System.out.println("Day: " + ( i + 1));
+			while(constructed.contains(maxHeap.peek())) {
+				System.out.print(maxHeap.remove() + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ MainTransaction.java
@@ -0,0 +1,16 @@
+package com.prob2.driver;
+import com.prob2.service.*;
+
+public class MainTransaction {
+	public static void main(String[] args) {
+		BSTToRST tree = new BSTToRST();
+		tree.node = new Node(50);
+		tree.node.left = new Node(30);
+		tree.node.right = new Node(60);
+		tree.node.left.left = new Node(10);
+		tree.node.right.left = new Node(55);
+		
+		tree.bSTToRST(tree.node);
+		tree.printRST(tree.headNode);
+	}
+}
